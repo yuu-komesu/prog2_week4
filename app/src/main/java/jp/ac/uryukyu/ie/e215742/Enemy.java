b6c39bbb93a0f2e18bf33f0a340ff8d6d0c53ca1
Komesu YU <e215742@ie.u-ryukyu.ac.jp>
2021-11-13 11:23:01 +0900

    fix: enemy自身が死亡している状態で攻撃できる不具合の修正

--- app/src/main/java/jp/ac/uryukyu/ie/e215742/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215742/Enemy.java
@@ -34,9 +34,15 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if (dead == false){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            hero.wounded(damage);
+        }else{
+            int damage = 0;
+            System.out.printf("%s自身が死亡している状態では攻撃できない\n", name);
+            hero.wounded(damage);
+        }
     }
 
     /**
